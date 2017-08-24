6f5434175fc25864e10f34c8dd7e3b31ba5c8ce1
Matthew Peterson <matt2694@edu.eal.dk>
2017-08-24 14:53:42 +0200

    Exercise 4A done

---
+++ 24_8_17_Server_Excercise_4/HandleClient.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace _24_8_17_Server_Excercise_4
+{
+    internal class HandleClient
+    {
+        private TcpClient client;
+        private Socket clSock;
+        private StreamReader reader = null;
+        private StreamWriter writer = null;
+
+        public HandleClient(TcpClient newClient)
+        {
+            client = newClient;
+        }
+
+        public HandleClient(Socket newSocket)
+        {
+            clSock = newSocket;
+        }
+
+        public void Echo()
+        {
+            //using Sockets
+
+            try
+            {
+                IPEndPoint remoteIPEndPoint = clSock.RemoteEndPoint as IPEndPoint;
+                IPEndPoint localIPEndPoint = clSock.LocalEndPoint as IPEndPoint;
+
+                byte[] buffer = new byte[1024];
+                while (clSock.Receive(buffer) > 0)
+                {
+                    string str = Encoding.ASCII.GetString(buffer);
+                    byte[] msg = Encoding.ASCII.GetBytes(str);
+                    clSock.Send(msg);
+                }
+                clSock.Shutdown(SocketShutdown.Both);
+                clSock.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            //using Tcp
+
+            //try
+            //{
+            //    reader = new StreamReader(client.GetStream());
+            //    writer = new StreamWriter(client.GetStream());
+            //    writer.AutoFlush = true;
+
+            //    while (true)
+            //    {
+            //        string msg = reader.ReadLine();
+            //        writer.WriteLine(msg);
+            //    }
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine(e.Message);
+            //}
+        }
+    }
+}
--- 24_8_17_Server_Excercise_4/Program.cs
+++ 24_8_17_Server_Excercise_4/Program.cs
@@ -1,15 +1,96 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace _24_8_17_Server_Excercise_4
 {
     class Program
     {
+        private TcpListener listener = null;
+        Socket s = null;
+        private List<Thread> clientList = new List<Thread>();
+
         static void Main(string[] args)
         {
+            Program server = new Program();
+
+            //using Sockets
+
+            server.Run(IPAddress.Loopback, 5000);
+
+            //using Tcp
+
+            //server.Run();
+        }
+
+        private void Run(IPAddress ip, int port)
+        {
+            try
+            {
+                s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                s.Bind(new IPEndPoint(ip, port));
+                s.Listen(10);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+            Console.WriteLine("Ready");
+            ConnectClients();
+        }
+
+        private void Run()
+        {
+            try
+            {
+                listener = new TcpListener(5000);
+                listener.Start();
+                Console.WriteLine("Ready");
+                ConnectClients();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (listener != null)
+                {
+                    listener.Stop();
+                }
+            }
+        }
+
+        private void ConnectClients()
+        {
+            //using Sockets
+
+            Console.WriteLine("Waiting for incoming client connections...");
+            HandleClient clientHandler;
+            while (true)
+            {
+                Socket newSocket = s.Accept();
+                clientHandler = new HandleClient(newSocket);
+                clientList.Add(new Thread(new ThreadStart(clientHandler.Echo)));
+                clientList[clientList.Count - 1].Start();
+            }
+
+            //using Tcp
+
+            //Console.WriteLine("Waiting for incoming client connections...");
+            //HandleClient clientHandler;
+            //while (true)
+            //{
+            //    clientHandler = new HandleClient(listener.AcceptTcpClient());
+            //    clientList.Add(new Thread(new ThreadStart(clientHandler.Echo)));
+            //    clientList[clientList.Count - 1].Start();
+            //}
         }
     }
 }
