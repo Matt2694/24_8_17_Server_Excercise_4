71184b75cef25b0467a56c6e429ebdf6ea1a0bbf
Matthew Peterson <matt2694@edu.eal.dk>
2017-08-24 12:48:45 +0200

    Add project files.

---
+++ 24_8_17_Server_Excercise_4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24_8_17_Server_Excercise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
