1c99e71bf4f2d7bc83627b575e8d03881d5e8869
Matthew Peterson <matt2694@edu.eal.dk>
2017-08-31 10:48:11 +0200

    added method for echoupper

---
+++ 24_8_17_Server_Excercise_4/EchoService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24_8_17_Server_Excercise_4
+{
+    public static class EchoService
+    {
+    }
+}
--- 24_8_17_Server_Excercise_4/HandleClient.cs
+++ 24_8_17_Server_Excercise_4/HandleClient.cs
@@ -23,48 +23,72 @@ namespace _24_8_17_Server_Excercise_4
             clSock = newSocket;
         }
 
-        public void Echo()
+        private void DoDialog()
         {
-            //using Sockets
 
+        }
+
+        private void ExecuteCommand()
+        {
+
+        }
+
+        private void ReceiveFromClient()
+        {
+
+        }
+
+        public void RunClient()
+        {
+
+        }
+
+        private void SendToClient()
+        {
+
+        }
+
+        public void EchoHandler()
+        {
             try
             {
-                IPEndPoint remoteIPEndPoint = clSock.RemoteEndPoint as IPEndPoint;
-                IPEndPoint localIPEndPoint = clSock.LocalEndPoint as IPEndPoint;
+                reader = new StreamReader(client.GetStream());
+                writer = new StreamWriter(client.GetStream());
+                writer.AutoFlush = true;
 
-                byte[] buffer = new byte[1024];
-                while (clSock.Receive(buffer) > 0)
+                while (true)
                 {
-                    string str = Encoding.ASCII.GetString(buffer);
-                    byte[] msg = Encoding.ASCII.GetBytes(str);
-                    clSock.Send(msg);
+                    string msg = reader.ReadLine();
+                    string[] commandArray = msg.Split(' ');
+                    if(commandArray[0] == "Echo" || commandArray[0] == "echo")
+                    {
+                        msg = Echo(commandArray[1]);
+                    }
+                    else if (commandArray[0] == "EchoUpper" || commandArray[0] == "echoupper")
+                    {
+                        msg = EchoUpper(commandArray[1]);
+                    }
+                    else
+                    {
+                        msg = "Invalid Command";
+                    }
+                    writer.WriteLine(msg);
                 }
-                clSock.Shutdown(SocketShutdown.Both);
-                clSock.Close();
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
+        }
 
-            //using Tcp
+        public string Echo(string input)
+        {
+            return input;
+        }
 
-            //try
-            //{
-            //    reader = new StreamReader(client.GetStream());
-            //    writer = new StreamWriter(client.GetStream());
-            //    writer.AutoFlush = true;
-
-            //    while (true)
-            //    {
-            //        string msg = reader.ReadLine();
-            //        writer.WriteLine(msg);
-            //    }
-            //}
-            //catch (Exception e)
-            //{
-            //    Console.WriteLine(e.Message);
-            //}
+        public string EchoUpper(string input)
+        {
+            return input.ToUpper();
         }
     }
 }
